da88b7f98b6325a33e10c42bdfada9c07ee764bb
Hippolyte Bach <hbach@juniorisep.com>
2022-04-04 23:25:22 +0200

    cors policy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ServiceAccount } from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: '*' });
   const config = new DocumentBuilder()
     .setTitle('Gravity')
     .setDescription('Gravity')
